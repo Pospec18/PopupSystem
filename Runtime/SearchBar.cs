4881477edd75fdf89ff097935e95d7474461a779
Jan Pospíšil <pospij52@fit.cvut.cz>
2023-06-25 22:23:22 +0200

    Rename Search files and fix bugs

--- Runtime/SearchBar.cs
+++ Runtime/SearchBar.cs
@@ -1,85 +1,61 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEngine;
 
 namespace Pospec.Popup
 {
-    public class SearchBar<T>
+    public class SearchBar : MonoBehaviour
     {
-        private readonly IDictionary<char, ISearchNode> items = new Dictionary<char, ISearchNode>();
+        private readonly SearchBarTree<Action> searchTree = new SearchBarTree<Action>();
+        private readonly List<PopupButton> searchList = new List<PopupButton>();
 
-        private interface ISearchNode
+        [SerializeField] private Transform resultsTab;
+        [SerializeField] private GameObject scrollView;
+        [SerializeField] private PopupButton resultPrefab;
+        [SerializeField] private TMPro.TMP_InputField searchField;
+
+        private void Start()
         {
-            IEnumerable<SearchResult<T>> GetValues(string key);
-            void Add(string key, SearchResult<T> value);
+            searchField.onSubmit.AddListener(Use);
+            Close();
         }
 
-        private class SearchNode : ISearchNode
+        public void Add(string key, Action onSelected)
         {
+            searchTree.Add(key, onSelected);
+        }
 
-            private readonly IList<KeyValuePair<string, SearchResult<T>>> lines = new List<KeyValuePair<string, SearchResult<T>>>();
-            private static readonly char[] escapeCharacters = { ':', '-' };
-
-            public IEnumerable<SearchResult<T>> GetValues(string key)
+        public void Use(string key)
+        {
+            Reset();
+            scrollView.SetActive(true);
+            foreach (var item in searchTree.Find(key))
             {
-                List<SearchResult<T>> startOfLine = new List<SearchResult<T>>();
-                List<SearchResult<T>> startOfWord = new List<SearchResult<T>>();
-                List<SearchResult<T>> startOfSomething = new List<SearchResult<T>>();
-                List<SearchResult<T>> inMiddle = new List<SearchResult<T>>();
-                foreach (var line in lines)
+                PopupButton button = Instantiate(resultPrefab, resultsTab);
+                button.textField.text = item.Key;
+                button.button.onClick.AddListener(() =>
                 {
-                    if (!line.Key.EndsWith(key))
-                        continue;
-
-                    int lineLength = line.Key.Length;
-                    int keyLength = key.Length;
-                    if (lineLength == keyLength)
-                        startOfLine.Add(line.Value);
-                    else if (line.Key[lineLength - keyLength - 1] == ' ')
-                        startOfWord.Add(line.Value);
-                    else if (escapeCharacters.Contains(line.Key[lineLength - keyLength - 1]))
-                        startOfSomething.Add(line.Value);
-                    else
-                        inMiddle.Add(line.Value);
-                }
-
-                return startOfLine.Union(startOfWord).Union(startOfSomething).Union(inMiddle);
+                    item.Value();
+                    Close();
+                });
+                searchList.Add(button);
             }
+        }
 
-            public void Add(string key, SearchResult<T> value)
+        protected void Reset()
+        {
+            foreach (var item in searchList)
             {
-                lines.Add(KeyValuePair.Create(key, value));
+                Destroy(item.gameObject);
             }
+
+            searchList.Clear();
         }
 
-        public void Add(string key, T value)
+        public void Close()
         {
-            string keyLower = key.ToLower();
-            SearchResult<T> searchValue = new SearchResult<T>(key, value);
-            for (int i = 0; i < keyLower.Length; i++)
-            {
-                items.TryAdd(keyLower[i], new SearchNode());
-                items[keyLower[i]].Add(keyLower.Substring(0, i + 1), searchValue);
-            }
-        }
-
-        public IEnumerable<SearchResult<T>> Find(string key)
-        {
-            string keyLower = key.ToLower();
-            if (items.TryGetValue(keyLower[keyLower.Length - 1], out ISearchNode node))
-                return node.GetValues(keyLower);
-            return null;
-        }
-    }
-
-    public class SearchResult<T>
-    {
-        public string Key { get; private set; }
-        public T Value { get; private set; }
-
-        public SearchResult(string key, T value)
-        {
-            Key = key;
-            Value = value;
+            scrollView.SetActive(false);
         }
     }
 }
---
+++ Runtime/SearchBarTree.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pospec.Popup
+{
+    internal class SearchBarTree<T>
+    {
+        private readonly IDictionary<char, ISearchNode> items = new Dictionary<char, ISearchNode>();
+
+        private interface ISearchNode
+        {
+            IEnumerable<SearchResult<T>> GetValues(string key);
+            void Add(string key, SearchResult<T> value);
+        }
+
+        private class SearchNode : ISearchNode
+        {
+
+            private readonly IList<KeyValuePair<string, SearchResult<T>>> lines = new List<KeyValuePair<string, SearchResult<T>>>();
+            private static readonly char[] escapeCharacters = { ':', '-', '.', '/', '_' };
+
+            public IEnumerable<SearchResult<T>> GetValues(string key)
+            {
+                List<SearchResult<T>> startOfLine = new List<SearchResult<T>>();
+                List<SearchResult<T>> startOfWord = new List<SearchResult<T>>();
+                List<SearchResult<T>> startOfSomething = new List<SearchResult<T>>();
+                List<SearchResult<T>> inMiddle = new List<SearchResult<T>>();
+                foreach (var line in lines)
+                {
+                    if (!line.Key.EndsWith(key))
+                        continue;
+
+                    int lineLength = line.Key.Length;
+                    int keyLength = key.Length;
+                    if (lineLength == keyLength)
+                        startOfLine.Add(line.Value);
+                    else if (line.Key[lineLength - keyLength - 1] == ' ')
+                        startOfWord.Add(line.Value);
+                    else if (escapeCharacters.Contains(line.Key[lineLength - keyLength - 1]))
+                        startOfSomething.Add(line.Value);
+                    else
+                        inMiddle.Add(line.Value);
+                }
+
+                return startOfLine.Union(startOfWord).Union(startOfSomething).Union(inMiddle);
+            }
+
+            public void Add(string key, SearchResult<T> value)
+            {
+                lines.Add(KeyValuePair.Create(key, value));
+            }
+        }
+
+        public void Add(string key, T value)
+        {
+            string keyLower = key.ToLower();
+            SearchResult<T> searchValue = new SearchResult<T>(key, value);
+            for (int i = 0; i < keyLower.Length; i++)
+            {
+                items.TryAdd(keyLower[i], new SearchNode());
+                items[keyLower[i]].Add(keyLower.Substring(0, i + 1), searchValue);
+            }
+        }
+
+        public IEnumerable<SearchResult<T>> Find(string key)
+        {
+            string keyLower = key.ToLower();
+            if (keyLower.Length > 0 && items.TryGetValue(keyLower[keyLower.Length - 1], out ISearchNode node))
+                return node.GetValues(keyLower);
+            return Enumerable.Empty<SearchResult<T>>();
+        }
+    }
+
+    internal class SearchResult<T>
+    {
+        public string Key { get; private set; }
+        public T Value { get; private set; }
+
+        public SearchResult(string key, T value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
